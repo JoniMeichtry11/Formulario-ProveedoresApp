c946b51f47bc5a8016cb6a70b088ca0de2e6a51b
JoniMeichtry11 <joni.meichtry@gmail.com>
2025-11-05 13:31:21 -0300

    feat: add results page finish

--- src/app/pages/results/results.ts
+++ src/app/pages/results/results.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+interface Formulario {
+  nombre: string;
+  empresa: string;
+  telefono: string;
+  email: string;
+  interes: string;
+}
 
 @Component({
   selector: 'app-results',
@@ -6,6 +16,25 @@ import { Component } from '@angular/core';
   templateUrl: './results.html',
   styleUrl: './results.scss',
 })
-export class Results {
+export class Results implements OnInit {
+  formularios = signal<Formulario[]>([]);
+  loading = signal(true);
 
+  constructor(private firestore: Firestore) {}
+
+  ngOnInit(): void {
+    const ref = collection(this.firestore, 'formularios');
+    const obs: Observable<Formulario[]> = collectionData(ref, { idField: 'id' }) as Observable<Formulario[]>;
+
+    obs.subscribe({
+      next: data => {
+        this.formularios.set(data);
+        this.loading.set(false);
+      },
+      error: err => {
+        console.error('Error al cargar formularios', err);
+        this.loading.set(false);
+      }
+    });
+  }
 }
